6ba7595d99d1d1ecf86d0187b3140af44900bcc8
nhan.nguyen <nhan.nguyen@uac.edu.au>
2023-06-30 10:10:52 +0700

    up

--- src/_views/LoginCard.tsx
+++ src/_views/LoginCard.tsx
@@ -2,13 +2,14 @@
 
 import React, {useState} from "react";
 import {signIn} from "@/hooks/useUser";
-import {Button, FluidForm, Form, TextInput, InlineLoading} from "@carbon/react";
+import {Button, FluidForm, Form, TextInput, InlineLoading, InlineNotification, ToastNotification} from "@carbon/react";
 import {useForm} from "react-hook-form";
 import {ButtonWithLoading} from "@/_components/Buttons";
 
 
 export const LoginCard = () => {
   const [submitting, setSubmitting] = useState(false);
+  const [error, setError] = useState<string | undefined>(undefined);
   const form = useForm<{
     email: string,
     password: string,
@@ -17,9 +18,8 @@ export const LoginCard = () => {
     <FluidForm className={''}
                onSubmit={form.handleSubmit(async (data, event) => {
                  setSubmitting(true)
-                 console.log(data);
                  const [user, error] = await signIn(data.email, data.password)
-                 console.log({user, error})
+                 if (error) setError(error);
                  setSubmitting(false)
                })}
     >
@@ -32,7 +32,10 @@ export const LoginCard = () => {
                  placeholder={'Enter your password'}
                  id={'password'}
                  labelText={'Password'}/>
-      <ButtonWithLoading type={'submit'} loading={submitting} loadingMessage={'Signing in...'}>Login</ButtonWithLoading>
+      <div className={'md:flex'}>
+        <ButtonWithLoading type={'submit'} loading={submitting} loadingMessage={'Signing in...'}>Login</ButtonWithLoading>
+        {error && <Button kind={'danger--ghost'} onClick={() => setError(undefined)}>{error}</Button>}
+      </div>
     </FluidForm>
   </>
 }
