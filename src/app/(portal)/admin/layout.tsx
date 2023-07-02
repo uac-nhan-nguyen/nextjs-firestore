7fe0375ddb930eb15ff50f9cf9c04a8f609c52b7
Nhan Nguyen <nhan.nguyen@uac.edu.au>
2023-07-02 14:33:29 +0700

    up

--- src/app/(portal)/admin/layout.tsx
+++ src/app/(portal)/admin/layout.tsx
@@ -1,17 +1,35 @@
 'use client'
-import {UIShell} from "@/_components/carbon/UIShell";
-import {useUser} from "@/hooks/useUser";
+import { UIShell } from "@/_components/carbon/UIShell";
+import { useUser } from "@/hooks/useUser";
+import { useEffect } from "react";
+import { useRouter } from 'next/navigation'
 
 export default function Layout() {
-  const {user} = useUser();
+  const { user } = useUser();
+  const router = useRouter()
+
+  useEffect(() => {
+    if (user === null) {
+      router.push('/login')
+    }
+  }, [user, router])
+
+  if (user === undefined) {
+    return <div>Loading...</div>
+  }
+
+  if (user === null){
+    return <div>Not login. Redirecting to login page...</div>
+  }
+
   return <>
     <UIShell title={'Admin'}
-             user={user && {
-               email: user.email || undefined
-             } || undefined}
-             sidebarMenus={[
-               {label: 'Pages', href: '/admin/pages'}
-             ]}>
+      user={user && {
+        email: user.email || undefined
+      } || undefined}
+      sidebarMenus={[
+        { label: 'Pages', href: '/admin/pages' }
+      ]}>
       children
     </UIShell>
   </>
